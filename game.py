37d4215f1bb70b6bb3654b9c4682df79b987e7c6
Harshit Maheshwari <harshitmah98@gmail.com>
2019-02-15 12:58:17 +0530

    Model built

--- game.py
+++ game.py
@@ -1,11 +1,23 @@
 import os
 import time
-
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 import numpy as np
 import pandas as pd
+from PIL import ImageGrab #grabbing image
+from PIL import Image
+import cv2 #opencv
+from keras.models import model_from_json
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.optimizers import SGD , Adam
+from keras.callbacks import TensorBoard
+from collections import deque
+import random
+import pickle
+import json
 
 
 class Game:
@@ -67,4 +79,63 @@ class DinoAgent:
 
     def duck(self):
         self._game.press_down()
+
+
+class GameState:
+    def __init__(self, agent, game):
+        self._game = game
+        self_agent = agent
+
+    def get_state(self, actions):
+        score = self._game.get_score()
+        reward = 0.1*score/10
+        is_over = False
+        if actions[1] == 1:
+            self._agent.jump()
+            reward = 0.1*reward/11
+        image = grab_screen()
+
+        if self._agent.is_crashed():
+            self._game.restart()
+            reward = -11/score
+            is_over = True
+
+
+def grab_screen(_driver = None):
+    screen = np.array(ImageGrab.grab(bbox=(40, 180, 440, 400)))
+    image = process_img(screen)
+    return image
+
+
+def process_img(image):
+    image = cv2.resize(image, (0, 0), fx=0.15, fy=0.10)
+    image = image[2:38, 10:50]
+    image = cv2.Canny(image, threshold1=100, threshold2=200)
+    return image
+
+
+LEARNING_RATE = 1e-4
+img_rows, img_cols = 40, 20
+img_channels = 4
+ACTIONS = 2
+
+
+def build_model():
+    print("Now we build the model")
+    model = Sequential()
+    model.add(Conv2D(32, (8, 8), strides=(4, 4), padding='same', input_shape=(img_cols, img_rows, img_channels)))  # 20*40*4
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (4, 4), strides=(2, 2), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3), strides=(1, 1), padding='same'))
+    model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dense(ACTIONS))
+    adam = Adam(lr=LEARNING_RATE)
+    model.compile(loss='mse', optimizer=adam)
+    print("We finish building the model")
+    return model
+
 
